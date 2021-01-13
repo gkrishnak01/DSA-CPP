925165cc4b84e1c3ec95eb04c649a13c28351942
Gautham Krishna K <gkrishnak2001@gmail.com>
2021-01-13 06:30:00 +0530

    Cyclic rotation of array

---
+++ Arrays/cyclicRotation.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using namespace std;
+
+void rotate(int arr[],int size)
+{
+    int temp = arr[size - 1];
+    for(int i = size - 1; i >= 0; i--)
+    {
+        arr[i + 1] = arr [i];
+    }
+    arr[0] = temp;
+}
+
+void printArray(int arr[],int size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
+
+int main()
+{
+    int arr[] = {9, 8, 7, 6, 4, 2, 1, 3};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    rotate(arr,size);
+    printArray(arr,size);
+    return 0;
+}
