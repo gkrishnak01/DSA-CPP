c5e22847d10f0c15ebdeef567811f49ac316a37e
Gautham Krishna K <gkrishnak2001@gmail.com>
2021-01-14 06:32:22 +0530

    Minimum Jumps to end of Array

---
+++ Arrays/jumpsToEndOfAnArray.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int jumps(int arr[],int size)
+{
+    int jumps = 0;
+    int i = 0;
+    while(i < size - 1)
+    {   
+        jumps++;
+        i += arr[i];
+    }
+    return jumps;
+}
+
+int main()
+{
+    int arr[] = {1,3,4,2,6,7};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    cout<<jumps(arr,size);
+    return 0;
+}
